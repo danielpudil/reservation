4cf9ec6392e91e09bf4485939cab47027eecdc58
Daniel Pudil <daniel-pudil@email.cz>
2017-12-23 12:29:15 +0100

    Changed password from admin

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,6 +1,6 @@
 module Admin
   class AdminController < ApplicationController
     layout "admin"
-    http_basic_authenticate_with name: 'admin', password: 'admin'
+    http_basic_authenticate_with name: 'unicorn', password: 'bmw'
   end
 end
